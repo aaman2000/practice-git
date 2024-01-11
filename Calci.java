07411e1d6f040521defeb1006516d8cae3c0f66b
aaman2000 <aamanhzb8863@gmail.com>
2024-01-11 11:36:20 +0530

    it is implent

--- Calci.java
+++ Calci.java
@@ -2,3 +2,12 @@ interface Calci{
  public int getSum(int a,int b);
  public int getProduct(int a,int b);
 }
+class Calciimp implements Calci{
+  public int getSum(int a,int b){
+    return(a+b);
+  }
+  public int getProduct(int a,int b){
+    return(a*b);
+
+}
+}
